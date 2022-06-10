c956497902dc8a7d7da7c10991c5e94c0ffa0cce
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2022-06-10 20:46:28 +0530

    update server url

--- src/components/config/Config.js
+++ src/components/config/Config.js
@@ -4,7 +4,12 @@ const hostname = window.location.hostname;
 //   CLIENT_URL: hostname == "localhost" ? "http://localhost:3000" : "",
 // };
 
+// export default {
+//   SERVER_URL: "http://54.201.100.185:5000/api/v1",
+//   CLIENT_URL: "http://54.201.100.185",
+// };
+
 export default {
-  SERVER_URL: "http://54.201.100.185:5000/api/v1",
+  SERVER_URL: "http://35.89.12.176/api/v1",
   CLIENT_URL: "http://54.201.100.185",
 };
--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,10 @@
 const hostname = window.location.hostname;
+// export default {
+//   SERVER_URL: "http://54.201.100.185:5000/api/v1",
+//   CLIENT_URL: "http://54.201.100.185",
+// };
+
 export default {
-  SERVER_URL: "http://54.201.100.185:5000/api/v1",
+  SERVER_URL: "http://35.89.12.176/api/v1",
   CLIENT_URL: "http://54.201.100.185",
 };
